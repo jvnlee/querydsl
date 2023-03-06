812883603e7f17ca010c84d02585b1a33f604187
jvnlee <lhjun1028@naver.com>
2023-03-06 14:37:39 +0900

    feat: Spring Data JPA 방식의 MemberRepository 인터페이스 추가
    기본 메서드 외에 findByUsername() 추가

---
+++ src/main/java/learn/querydsl/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package learn.querydsl.repository;
+
+import learn.querydsl.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsername(String username);
+
+}
