c0944fd96fdc8a784087ced4e48cf0493b35853e
jvnlee <lhjun1028@naver.com>
2023-03-07 05:50:39 +0900

    feat: searchWithPaging()의 count 쿼리 최적화
    반환 시 페이지를 생성하는 방식을 PageImpl 인스턴스 생성 대신 PageableExecutionUtils.getPage() 호출로 대체. 해당 메서드는 3번째 파라미터로 콜백을 받는데, 이 콜백은 count 쿼리가 필요하지 않은 경우에는 count 쿼리 호출을 생략해주는 기능을 함. 따라서 불필요한 경우에도 count를 하게 되는 일을 방지해 성능을 최적화할 수 있음.

--- src/main/java/learn/querydsl/repository/MemberCustomRepositoryImpl.java
+++ src/main/java/learn/querydsl/repository/MemberCustomRepositoryImpl.java
@@ -2,10 +2,12 @@ package learn.querydsl.repository;
 
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import learn.querydsl.dto.MemberSearchCond;
 import learn.querydsl.dto.MemberTeamDto;
 import learn.querydsl.dto.QMemberTeamDto;
+import learn.querydsl.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -71,7 +73,7 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
                 .fetch();
 
         // count 쿼리 (조건에 부합하는 로우의 총 개수를 얻는 것이기 때문에 페이징 미적용)
-        Long total = queryFactory
+        JPAQuery<Long> fetchQuery = queryFactory
                 .select(member.count()) // SQL 상으로는 count(member.id)와 동일
                 .from(member)
                 .leftJoin(member.team, team)
@@ -80,10 +82,17 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
                         teamNameEq(cond.getTeamName()),
                         ageGoe(cond.getAgeGoe()),
                         ageLoe(cond.getAgeLoe())
-                )
-                .fetchOne();
+                );
 
-        return new PageImpl<>(content, pageable, total);
+        /*
+         3번째 파라미터: () -> fetchQuery.fetchOne()
+         count 쿼리를 콜백으로 감싸서 필요한 경우에만 호출하도록 함 (lazy 호출)
+
+         count 쿼리가 필요하지 않은 경우:
+         1. 첫 페이지인데, 컨텐츠 사이즈가 페이지 사이즈보다 작을 때 (컨텐츠 사이즈가 곧 total 이므로 따로 구할 필요 없음)
+         2. 마지막 페이지일 때 ("offset + 컨텐츠 사이즈"가 곧 total 이므로 따로 구할 필요 없음)
+         */
+        return PageableExecutionUtils.getPage(content, pageable, fetchQuery::fetchOne);
     }
 
     private BooleanExpression usernameEq(String username) {
