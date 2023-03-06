77fcbada0e06d271777052c02396986c2b3dc2e0
jvnlee <lhjun1028@naver.com>
2023-03-07 04:59:21 +0900

    test: 페이징을 적용한 검색 메서드 테스트

--- src/test/java/learn/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/learn/querydsl/repository/MemberRepositoryTest.java
@@ -6,6 +6,8 @@ import learn.querydsl.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -44,5 +46,27 @@ class MemberRepositoryTest {
         List<MemberTeamDto> result = memberRepository.search(cond);
 
         assertThat(result).extracting("username").containsExactly("member3");
+    }
+
+    @Test
+    void searchWithPaging() {
+        Member member1 = new Member("member1", 10);
+        Member member2 = new Member("member2", 20);
+        Member member3 = new Member("member3", 30);
+        Member member4 = new Member("member4", 40);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+        memberRepository.save(member4);
+
+        MemberSearchCond cond = new MemberSearchCond(); // 검색 조건은 비우고 진행
+        PageRequest pageRequest = PageRequest.of(0, 3); // 0번 페이지, 크기 3
+
+        Page<MemberTeamDto> result = memberRepository.searchWithPaging(cond, pageRequest);
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent())
+                .extracting("username")
+                .containsExactly("member1", "member2", "member3");
     }
 }
