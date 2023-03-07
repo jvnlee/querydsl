d564a8cfe4ec4283ed81696135a37f19e5bcf82a
jvnlee <lhjun1028@naver.com>
2023-03-07 18:13:32 +0900

    feat: MemberRepository에 QueryDslPredicateExectuor 인터페이스 상속 추가
    QueryDslPredicateExectuor는 Spring Data JPA처럼 기본 조회 메서드와 카운트 메서드를 제공하고, 파라미터로 Predicate을 받을 수 있게 함. 즉 where 절에 들어갈 조건식을 메서드 파라미터로 넘길 수 있음.

--- src/main/java/learn/querydsl/repository/MemberRepository.java
+++ src/main/java/learn/querydsl/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package learn.querydsl.repository;
 
 import learn.querydsl.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);
 
